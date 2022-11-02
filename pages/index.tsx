b1ead045d0b78a196fb80be819c8859b67e44af2
mitrang <mitrang.lad@smartsensesolutions.com>
2022-11-02 18:44:40 +0530

    web3 provider context wrapping

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,36 @@
 /* eslint-disable @next/next/no-img-element */
 
 import type { NextPage } from "next"
-// import { useWeb3 } from "../components/providers/web3"
+import { useWeb3 } from "../components/providers/web3"
 import { BaseLayout, NftList } from "@ui"
 
 import nfts from "../content/meta.json"
 import { NftMeta } from "@_types/nft"
 
 const Home: NextPage = () => {
-    // const { test } = useWeb3()
+   const { test } = useWeb3()
 
-    return (
-        <BaseLayout>
-            {/* {test} */}
-            <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-                <div className="absolute inset-0">
-                    <div className="bg-white h-1/3 sm:h-2/3" />
-                </div>
-                <div className="relative">
-                    <div className="text-center">
-                        <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
-                            Amazing Creatures NFTs
-                        </h2>
-                        <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-                            Mint a NFT to get unlimited ownership forever!
-                        </p>
-                    </div>
-                    <NftList nfts={nfts as NftMeta[]} />
-                </div>
+   return (
+      <BaseLayout>
+         {console.log(test)}
+         <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+            <div className="absolute inset-0">
+               <div className="bg-white h-1/3 sm:h-2/3" />
             </div>
-        </BaseLayout>
-    )
+            <div className="relative">
+               <div className="text-center">
+                  <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+                     Amazing Creatures NFTs
+                  </h2>
+                  <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+                     Mint a NFT to get unlimited ownership forever!
+                  </p>
+               </div>
+               <NftList nfts={nfts as NftMeta[]} />
+            </div>
+         </div>
+      </BaseLayout>
+   )
 }
 
 export default Home
