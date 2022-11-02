cafe79f4f7a34473fccbbcf1f77916db3c4b8903
mitrang <mitrang.lad@smartsensesolutions.com>
2022-11-02 16:24:32 +0530

    folder structure refactored

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 
 import type { NextPage } from "next"
-import { BaseLayout, NftList } from "../components"
+import { BaseLayout, NftList } from "../components/ui"
 
 import nfts from "../content/meta.json"
 import { NftMeta } from "../types/nft"
