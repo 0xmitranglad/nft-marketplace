6a13ec2e989d24845ea825632be923d740650d40
mitrang <mitrang.lad@smartsensesolutions.com>
2022-11-02 18:28:30 +0530

    code refactor for errors, added pages and layout

--- components/ui/navbar/index.tsx
+++ components/ui/navbar/index.tsx
@@ -1,14 +1,13 @@
 /* eslint-disable @next/next/no-img-element */
 
-import { Fragment } from "react"
-import { Disclosure, Menu, Transition } from "@headlessui/react"
+import { Disclosure, Menu } from "@headlessui/react"
 import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline"
+import Link from "next/link"
+import ActiveLink from "../link"
 
 const navigation = [
-    { name: "Dashboard", href: "#", current: true },
-    { name: "Team", href: "#", current: false },
-    { name: "Projects", href: "#", current: false },
-    { name: "Calendar", href: "#", current: false },
+    { name: "Marketplace", href: "/", current: true },
+    { name: "Create", href: "/nft/create", current: false },
 ]
 
 function classNames(...classes: string[]) {
@@ -44,19 +43,18 @@ export default function Navbar() {
                                 <div className="hidden sm:block sm:ml-6">
                                     <div className="flex space-x-4">
                                         {navigation.map((item) => (
-                                            <a
+                                            <ActiveLink
                                                 key={item.name}
                                                 href={item.href}
-                                                className={classNames(
-                                                    item.current
-                                                        ? "bg-gray-900 text-white"
-                                                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                                                    "px-3 py-2 rounded-md text-sm font-medium"
-                                                )}
-                                                aria-current={item.current ? "page" : undefined}
+                                                activeClass="bg-gray-900 text-white"
                                             >
-                                                {item.name}
-                                            </a>
+                                                <a
+                                                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                                    aria-current={item.current ? "page" : undefined}
+                                                >
+                                                    {item.name}
+                                                </a>
+                                            </ActiveLink>
                                         ))}
                                     </div>
                                 </div>
@@ -71,7 +69,7 @@ export default function Navbar() {
                                 </button>
 
                                 {/* Profile dropdown */}
-                                <Menu as="div" className="ml-3 relative">
+                                <Menu as="div" className="ml-3 relative z-10">
                                     <div>
                                         <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                                             <span className="sr-only">Open user menu</span>
@@ -82,57 +80,22 @@ export default function Navbar() {
                                             />
                                         </Menu.Button>
                                     </div>
-                                    <Transition
-                                        as={Fragment}
-                                        enter="transition ease-out duration-100"
-                                        enterFrom="transform opacity-0 scale-95"
-                                        enterTo="transform opacity-100 scale-100"
-                                        leave="transition ease-in duration-75"
-                                        leaveFrom="transform opacity-100 scale-100"
-                                        leaveTo="transform opacity-0 scale-95"
-                                    >
-                                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href="#"
-                                                        className={classNames(
-                                                            active ? "bg-gray-100" : "",
-                                                            "block px-4 py-2 text-sm text-gray-700"
-                                                        )}
-                                                    >
-                                                        Your Profile
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href="#"
-                                                        className={classNames(
-                                                            active ? "bg-gray-100" : "",
-                                                            "block px-4 py-2 text-sm text-gray-700"
-                                                        )}
-                                                    >
-                                                        Settings
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <a
-                                                        href="#"
-                                                        className={classNames(
-                                                            active ? "bg-gray-100" : "",
-                                                            "block px-4 py-2 text-sm text-gray-700"
-                                                        )}
-                                                    >
-                                                        Sign out
-                                                    </a>
-                                                )}
-                                            </Menu.Item>
-                                        </Menu.Items>
-                                    </Transition>
+
+                                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                        <Menu.Item>
+                                            {({ active }) => (
+                                                <Link
+                                                    href="/profile"
+                                                    className={classNames(
+                                                        active ? "bg-gray-100" : "",
+                                                        "block px-4 py-2 text-sm text-gray-700"
+                                                    )}
+                                                >
+                                                    Your Profile
+                                                </Link>
+                                            )}
+                                        </Menu.Item>
+                                    </Menu.Items>
                                 </Menu>
                             </div>
                         </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+// import { Web3Provider } from "../components/providers"
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
+            {/* <Web3Provider> */}
             <Component {...pageProps} />
+            {/* </Web3Provider> */}
         </>
     )
 }
