cf0c747dae1427b5c1587328b068b48dfb5872b2
mitrang <mitrang.lad@smartsensesolutions.com>
2022-11-02 15:12:36 +0530

    Loop through nfts

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@
 import type { NextPage } from "next"
 import { BaseLayout, NftList } from "../components"
 
+import nfts from "../content/meta.json"
+
 const Home: NextPage = () => {
     return (
         <BaseLayout>
@@ -19,7 +21,7 @@ const Home: NextPage = () => {
                             Mint a NFT to get unlimited ownership forever!
                         </p>
                     </div>
-                    <NftList />
+                    <NftList nfts={nfts} />
                 </div>
             </div>
         </BaseLayout>
